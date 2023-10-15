a8fab8296b9058ca20a4b16cd38ca8d60c4b41cc
Sparsh Anand <114505404+sparsh-anand007@users.noreply.github.com>
2023-10-15 22:59:44 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/sparsh-anand007",
+    "name": "Sparsh Anand",
+    "role": "Full Stack Developer and Data Analyst",
+    "linkedinId":"https://www.linkedin.com/in/sparsh-anand-66853b1b5/",
+    "roleDescription": "I am a Pre-final year student at Vellore Institute of Technology, where I am pursuing a Bachelor of Technology in Computer Science, with a focus on data structures, algorithms, and object-oriented programming.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sparsh-anand-66853b1b5/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/sparsh-anand007",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Vellore Institute of Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"sparsh.anand007@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The Vellore Insitute of Technology",
+        "specialization":"Computer Science Engineering",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2025",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Vserv Infosystem Private Ltd.",
+        "specialization":"Software Development Intern",
+        "Duration":"Aug-September(2 Months)",
+        "YearOfLeaving":"2023",
+      },
+    ],
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Expressjs"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Web3"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"HTML & CSS",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+        "url":"https://sparsh-anand007.github.io/html-mern/"
+      },
+      {
+        "name":"Project2",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project3",
+        "description":"mobileapp",  
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project4",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Your designing skills are truly impressive and bring a unique and creative touch to everything you create.",
+        "name":"Arnav Goenka"
+      },
+      {
+        "description":"Your web development skills are impressive and consistently deliver outstanding results.",
+        "name":"Juan Mark Deen"
+      }
+    ]
+  }
+  
+  export default resumeData
